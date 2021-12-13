a5eb7102bdd3d74d7d4860bab3a837cb6b6e216c
LeeHakJoon <2hakjoon@gmail.com>
2021-12-13 12:15:42 +0900

    add default value to type orm and graphql

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,5 +1,5 @@
 import { Field, ObjectType } from "@nestjs/graphql";
-import { IsBoolean, IsString, Length } from "class-validator";
+import { IsBoolean, IsOptional, IsString, Length } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @ObjectType()
@@ -15,8 +15,9 @@ export class Restaurant {
   @Length(3, 10)
   name: string;
 
-  @Field((type) => Boolean, { nullable: true })
-  @Column()
+  @Field((type) => Boolean, { defaultValue: true })
+  @Column({ default: true })
+  @IsOptional()
   @IsBoolean()
   isVegan?: boolean;
 
