2cb5c1b33a87e70f346a927f2090b1cd076ee7a1
LeeHakJoon <2hakjoon@gmail.com>
2021-12-14 22:24:48 +0900

    add resolver find user profile

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -7,6 +7,7 @@ import {
   CreateAccountOutput,
 } from './dtos/create-account.dto';
 import { LoginInput, LoginOutput } from './dtos/login.dto';
+import { UserProfileInput, UserProfileOutput } from './dtos/user-profile.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
@@ -47,5 +48,31 @@ export class UsersResolver {
   @UseGuards(AuthGuard)
   me(@AuthUser() authUser: User) {
     return authUser;
+  }
+
+  @UseGuards(AuthGuard)
+  @Query((returns) => UserProfileOutput)
+  async userProfile(
+    @Args() userProfileInput: UserProfileInput,
+  ): Promise<UserProfileOutput> {
+    try {
+      const user = await this.usersService.findUserById(
+        userProfileInput.userId,
+      );
+      if (!user) {
+        throw Error();
+      }
+      if (user) {
+        return {
+          ok: true,
+          user,
+        };
+      }
+    } catch (e) {
+      return {
+        ok: false,
+        error: 'User Not Found',
+      };
+    }
   }
 }
