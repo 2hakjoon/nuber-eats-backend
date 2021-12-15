ef56dbbdca7e835c734bd2d18a6cf5f41a6b224f
LeeHakJoon <2hakjoon@gmail.com>
2021-12-15 18:36:12 +0900

    sending mail test is done

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,6 +1,7 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { MailModuleOptions } from './mail.interfaces';
+import { MailService } from './mail.service';
 
 @Module({})
 export class MailModule {
@@ -8,7 +9,7 @@ export class MailModule {
     return {
       module: MailModule,
       exports: [],
-      providers: [{ provide: CONFIG_OPTIONS, useValue: options }],
+      providers: [{ provide: CONFIG_OPTIONS, useValue: options }, MailService],
     };
   }
 }
