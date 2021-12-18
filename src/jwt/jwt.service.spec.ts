5ea7ddae1f09e86d0bdd24c5ed1dfa3ddbe6ab1f
LeeHakJoon <2hakjoon@gmail.com>
2021-12-18 16:52:40 +0900

    Testing code for jwt sign function

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,14 +1,17 @@
 import { Test } from '@nestjs/testing';
+import * as jwt from 'jsonwebtoken';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from './jwt.service';
 
 jest.mock('jsonwebtoken', () => {
   return {
-    sign: jest.fn(() => 'TOKEN'),
+    sign: jest.fn(() => TOKEN),
     verify: jest.fn(() => {}),
   };
 });
+const TOKEN = 'TOKEN';
 const TEST_KEY = 'testKey';
+const ID = 1;
 
 describe('JwtService', () => {
   let service: JwtService;
@@ -29,8 +32,10 @@ describe('JwtService', () => {
   });
   describe('sign', () => {
     it('should return a signed token', () => {
-      const token = service.sign(1);
-      console.log(token);
+      const token = service.sign(ID);
+      expect(token).toEqual(TOKEN);
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenCalledWith({ id: ID }, TEST_KEY);
     });
   });
   it.todo('verify');
