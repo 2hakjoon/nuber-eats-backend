913c445f03090b17744cee8b51c8f606de8fb2e6
LeeHakJoon <2hakjoon@gmail.com>
2021-12-19 01:01:37 +0900

    Testing code for creating user account

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,7 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -20,7 +23,34 @@ describe('AppController (e2e)', () => {
     app.close();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    const EMAIL = '2hakjoon@gmail.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation{
+            createAccount(input:{
+              email:"${EMAIL}",
+              password : "12345",
+              role: Client,
+            }){
+              ok,
+              error
+            }
+          }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+    it.todo('should fail if account already exsist');
+  });
+
   it.todo('userProfile');
   it.todo('login');
   it.todo('verifyEmail');
