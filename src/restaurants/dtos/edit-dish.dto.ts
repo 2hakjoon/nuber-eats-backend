a4972f042b21cd144bc53817ccbd72b8d58d395b
LeeHakJoon <2hakjoon@gmail.com>
2021-12-22 14:52:10 +0900

    add edit and delete dto of dish

---
+++ src/restaurants/dtos/edit-dish.dto.ts
@@ -0,0 +1,14 @@
+import { InputType, ObjectType, PartialType, PickType } from '@nestjs/graphql';
+import { CoreOutput } from 'src/common/dtos/output.dto';
+import { Dish } from '../entities/dish.entity';
+
+@InputType()
+export class EditDishInput extends PickType(PartialType(Dish), [
+  'name',
+  'options',
+  'price',
+  'description',
+]) {}
+
+@ObjectType()
+export class EditDishOutput extends CoreOutput {}
