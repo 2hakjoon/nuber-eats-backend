6935a5fc34f3b005432af985897093c5c1ce66c1
LeeHakJoon <2hakjoon@gmail.com>
2021-12-26 12:24:20 +0900

    generate payment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,6 +17,7 @@ import { Dish } from './restaurants/entities/dish.entity';
 import { OrdersModule } from './orders/orders.module';
 import { Order } from './orders/entities/order.entity';
 import { OrderItem } from './orders/entities/order-item.entity';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
   imports: [
@@ -87,6 +88,7 @@ import { OrderItem } from './orders/entities/order-item.entity';
     UsersModule,
     RestaurantsModule,
     OrdersModule,
+    PaymentsModule,
   ],
   controllers: [],
   providers: [],
