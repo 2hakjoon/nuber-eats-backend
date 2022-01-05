d63fdfd9219ff5e21f3232b85cde8ab3072e62de
LeeHakJoon <2hakjoon@gmail.com>
2022-01-05 23:11:07 +0900

    add upload module

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,7 @@ import { OrderItem } from './orders/entities/order-item.entity';
 import { PaymentsModule } from './payments/payments.module';
 import { Payment } from './payments/entities/payment.entity';
 import { ScheduleModule } from '@nestjs/schedule';
+import { UploadsModule } from './uploads/uploads.module';
 
 @Module({
   imports: [
@@ -93,6 +94,7 @@ import { ScheduleModule } from '@nestjs/schedule';
     RestaurantsModule,
     OrdersModule,
     PaymentsModule,
+    UploadsModule,
   ],
   controllers: [],
   providers: [],
