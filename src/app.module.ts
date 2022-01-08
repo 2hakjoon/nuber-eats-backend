abc6a6f8bf2c780345c6f94fa0872c3e8ee8ee3a
LeeHakJoon <2hakjoon@gmail.com>
2022-01-08 22:36:29 +0900

    change env values of database to url

--- src/app.module.ts
+++ src/app.module.ts
@@ -30,11 +30,11 @@ import { UploadsModule } from './uploads/uploads.module';
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'production', 'test').required(),
-        DB_HOST: Joi.string().required(),
-        DB_PORT: Joi.string().required(),
-        DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().required(),
-        DB_DATABASE: Joi.string().required(),
+        DB_HOST: Joi.string()
+        DB_PORT: Joi.string()
+        DB_USERNAME: Joi.string()
+        DB_PASSWORD: Joi.string()
+        DB_DATABASE: Joi.string()
         SECRET_KEY: Joi.string().required(),
         MAILGUN_API_KEY: Joi.string().required(),
         MAILGUN_DOMAIN_NAME: Joi.string().required(),
@@ -76,11 +76,15 @@ import { UploadsModule } from './uploads/uploads.module';
         CommonModule,
         Payment,
       ],
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      ...(process.env.DATABASE_URL 
+        ? {url:process.env.DATABASE_URL} 
+        : {
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+      })
     }),
     JwtModule.forRoot({
       secretKey: process.env.SECRET_KEY,
