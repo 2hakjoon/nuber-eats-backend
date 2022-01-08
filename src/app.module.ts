bfbcb748e95b90086e12763993402258acf359ac
LeeHakJoon <2hakjoon@gmail.com>
2022-01-08 22:38:52 +0900

    clean up codes

--- src/app.module.ts
+++ src/app.module.ts
@@ -76,15 +76,15 @@ import { UploadsModule } from './uploads/uploads.module';
         CommonModule,
         Payment,
       ],
-      ...(process.env.DATABASE_URL 
-        ? {url:process.env.DATABASE_URL} 
+      ...(process.env.DATABASE_URL
+        ? { url: process.env.DATABASE_URL }
         : {
-        host: process.env.DB_HOST,
-        port: +process.env.DB_PORT,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-      })
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+          }),
     }),
     JwtModule.forRoot({
       secretKey: process.env.SECRET_KEY,
